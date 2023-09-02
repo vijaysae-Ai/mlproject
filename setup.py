f530f3290ed49959061523e0e1566e6b729aa320
vijaysae-Ai <vijay.sae20@gmail.com>
2023-09-02 13:20:44 +0530

    Setup done

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup,find_packages
+
+setup(
+    name='mlprojects',
+    version='0.001',
+    author='Vijay',
+    author_email='vijay.sae20@gmail.com',
+    packages=find_packages(),
+    install_requires=['numpy','pandas','seaborn']
+)
